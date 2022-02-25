8068b59e6f631384545fde5319c6349647968616
brittany klose <brittany.klose@yahoo.com>
2022-02-25 16:34:14 -0500

    structuring functions for switch cases

--- index.js
+++ index.js
@@ -18,17 +18,36 @@ const startingPrompt = () => {
     ]).then(userChoice => {
         console.log(userChoice)
         switch(userChoice.menu){
+            case 'View All Employees':
+            viewAllEmployees();
+                break;
+            case 'Add Employee':
+                addEmployees();
+                break;
+            case 'Update Employee Role':
+                updateRole();
+                break;
+            case 'View All Roles':
+                viewAllRoles();
+                break;
+            case 'Add Role':
+                addRole();
+                break;  
             case 'View All Departments':
-            viewAllDepts();
-            break;
+                viewAllDepts();
+                break;      
+            case 'Add Department':
+                addDepartmment();
+                break;   
         }
     })
 }
 startingPrompt();
 
-//function
-function viewAllDepts(){
-    const sql = `SELECT id, dept_name AS title FROM departments`;
+
+// Function: VIEW all EMPLOYEES
+function viewAllEmployees(){
+    const sql = `SELECT id, first_name, last_name AS title FROM employees`;
     connection.query(sql, (err, rows) => {
       if (err) {
        console.log(error)
@@ -38,21 +57,56 @@ function viewAllDepts(){
       });
 }
 
-//if they pick add dept
-function addDepartmment() {
+// Function: to ADD an EMPLOYEE
+function addEmployees(){
 inquirer
 .prompt([
     {
         type: 'input',
-        message: 'What is the name of the department?',
-        name: 'dept',
+        message: "What is the employee's first name?",
+        name: 'first',
     },
-    //say dept name has been added to database
+    {
+        type: 'input',
+        message: "What is the employee's last name?",
+        name: 'last',
+    },
+    {
+        type: 'list',
+        message: "What is the employee's role?",
+        name: 'job',
+        choices: ["Account Manager", 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Sales Lead', 'Accountant', 'Legal Team Lead', 'Lawyer', 'Customer Service']
+    },
+    {
+        type: 'list',
+        message: "Who is the employee's manager?",
+        name: 'mngr',
+        choices: ["None", 'Michael Scott', 'John Snow', 'Taylor Swift', 'Ann Perkins', ]
+    },
+    // Say (employee name) has been added to database
     //then bring back to main menu
-])
-    .then(department.Data)
+]);
 }
-//if they pick add role
+
+// Function: UPDATE EMPLOYEE
+// function updateRole(){
+
+// }
+
+//Function: VIEW all ROLES
+function viewAllRoles(){
+    const sql = `SELECT id, title, salary AS title FROM roles`;
+    connection.query(sql, (err, rows) => {
+      if (err) {
+       console.log(error)
+         return;
+      }
+      console.table(rows)
+      });
+}
+
+//Function: to ADD a ROLE
+function addRole(){
 inquirer
 .prompt([
     {
@@ -75,35 +129,35 @@ inquirer
     //then bring back to main menu
     
 ]);
+}
 
-// If they pick add employee
+
+// Function: to VIEW all the DEPTS
+function viewAllDepts(){
+    const sql = `SELECT id, dept_name AS title FROM departments`;
+    connection.query(sql, (err, rows) => {
+      if (err) {
+       console.log(error)
+         return;
+      }
+      console.table(rows)
+      });
+}
+
+// Function: to ADD a DEPT
+function addDepartmment() {
 inquirer
 .prompt([
     {
         type: 'input',
-        message: "What is the employee's first name?",
-        name: 'first',
+        message: 'What is the name of the department?',
+        name: 'dept',
     },
-    {
-        type: 'input',
-        message: "What is the employee's last name?",
-        name: 'last',
-    },
-    {
-        type: 'list',
-        message: "What is the employee's role?",
-        name: 'job',
-        choices: ["Sales Lead", 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead', 'Lawyer', 'Customer Service']
-    },
-    {
-        type: 'list',
-        message: "Who is the employee's manager?",
-        name: 'mngr',
-        choices: ["None", 'John Doe', 'Michael Scott', 'David Smith', 'Bethany Hamilton', 'Michelle Obama', 'Haley Luciani']
-    },
-    // Say (employee name) has been added to database
+    //say dept name has been added to database
     //then bring back to main menu
-]);
-
-//if they pick view (employees, roles, depts) return tables from schema and seeds databases
-
+])
+    .then(departmentData =>{
+        console.log(departmentData)
+        //todo: db query to add dept goes here
+    })
+}
