7d8ca376b4f20db96d350b269641cacb850bd5bd
brittany klose <brittany.klose@yahoo.com>
2022-02-25 23:49:43 -0500

    organizing query functions

--- constructor.js
+++ constructor.js
@@ -1,8 +1,51 @@
 const connection = require('./connection.js')
 // --manager id null if employee has no manager
 // -- need iquirer, mysql2, and console.table pckg
-//---Should I make a query.sql file too?
-//--first is null cuz its a manager. next one has a manager
+
+// View All Roles
+
+//Add Role
+
+//Update employee Role
+app.put('/api/review/:id', (req, res) => {
+  const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
+  const params = [req.body.review, req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Movie not found'
+      });
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.affectedRows
+      });
+    }
+  });
+});
+
+
+//View All Employees
+  // Read list of all reviews and associated movie name using LEFT JOIN
+  app.get('/api/movie-reviews', (req, res) => {
+    const sql = `SELECT movies.movie_name AS movie, reviews.review FROM reviews LEFT JOIN movies ON reviews.movie_id = movies.id ORDER BY movies.movie_name;`;
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+//Add Employee
 
 // View all depts
     const sql = `SELECT id, dept_name AS title FROM departments`;
@@ -14,4 +57,20 @@ const connection = require('./connection.js')
       console.log(rows)
       });
    
+// Add a department
+// app.post('/api/new-dept', ({ body }, res) => {
+  const sql = `INSERT INTO departments (dept_name)
+    VALUES (?)`;
+  const params = [body.dept_name];
   
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+// });
