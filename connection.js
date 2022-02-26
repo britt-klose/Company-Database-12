f071014aca6a6fb81e69f0c45be0d722e848ce49
brittany klose <brittany.klose@yahoo.com>
2022-02-25 23:48:29 -0500

    deleted comments

--- connection.js
+++ connection.js
@@ -2,9 +2,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection(
     {
       host: 'localhost',
-      // MySQL username,
       user: 'root',
-      // TODO: Add MySQL password here
       password: 'Jasper69@omg',
       database: 'company_db'
     },
